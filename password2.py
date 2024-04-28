f7f58ad447e15d74300d70c8cb9b41a68b8fe750
Janani145 <123891896+Janani145@users.noreply.github.com>
2024-04-28 08:38:10 -0700

    Add files via upload

---
+++ password2.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+def generate_password(length, complexity):
+    if complexity == "1":
+        characters = string.ascii_letters + string.digits
+    elif complexity == "2":
+        characters = string.ascii_letters + string.digits + string.punctuation.replace("'", "").replace('"', '').replace("\\", "").replace("`", "").replace("^", "")
+    elif complexity == "3":
+        characters = string.printable.replace("'", "").replace('"', '').replace("\\", "").replace("`", "").replace("^", "")
+    else:
+        raise ValueError("Invalid complexity level. Please choose from 'easy', 'medium', or 'hard'.")
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    return password
+
+def main():
+    try:
+        length = int(input("Enter the desired length of the password: "))
+        complexity = input("Enter the complexity level(1/2/3) of the password \n 1.easy\n 2.medium\n 3.hard\nSelect the option :")
+        
+        password = generate_password(length, complexity)
+        print("Generated Password:", password)
+    except ValueError as e:
+        print(e)
+
+if __name__ == "__main__":
+    main()
